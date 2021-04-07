5aa3e9405c282c45705afc4ad06a009bdeef92ca
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-07 20:40:42 +0000

    pair prgramming with zack and james

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let col = 0; col < letters.length; col++) {
+    let verticalString = '';
+    for (let row = 0; row <= letters[col].length; row++) {
+      verticalString += letters[row][col];
     }
-}
-
-module.exports = wordSearch
+    if (verticalString === word) {
+      return true;
+    }
+  }
+  return false;
+};
+  
+module.exports = wordSearch;
